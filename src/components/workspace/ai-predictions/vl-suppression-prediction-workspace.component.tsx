c4b5e14066b06c52d2cc4b799291906561b3aa08
Daphne210 <dapnephinerry2@gmail.com>
2024-03-27 13:28:41 +0300

    Adding the components for VL suppression prediction

--- src/components/workspace/ai-predictions/vl-suppression-prediction-workspace.component.tsx
+++ src/components/workspace/ai-predictions/vl-suppression-prediction-workspace.component.tsx
@@ -1,33 +1,115 @@
-import React, { useEffect, useState } from "react";
-import { InlineLoading } from "@carbon/react";
+import React, { useEffect, useMemo, useState } from "react";
+import { InlineLoading, InlineNotification } from "@carbon/react";
 import styles from "./vl-suppression-prediction.scss";
 import {
+  extractDate,
+  useGetCurrentRegimen,
+  useGetAherence,
   useGetARTStartDate,
+  useGetLastEncounterDate,
+  useGetIndicationForVLTesting,
   useVLSuppressionDetails,
+  validateVLSuppressionParams,
 } from "./vl-suppression-prediction.resource";
-import { ErrorState } from "@openmrs/esm-framework";
+import logo from "../../../assets/images//artificial-intelligence-logo.png";
+import { PatientChartProps } from "../../../types";
+import { usePatient } from "@openmrs/esm-framework";
+import PatientList from "./vl-data-table.component";
 
-const VLSuppressionPredictionWorkSpace: React.FC = () => {
-  const [encounterDate, setEncounterDate] = useState("2023-04-20");
-  const [artStartDate, setArtStartDate] = useState("2020-04-20");
-  const [dateOfBirth, setDateOfBirth] = useState("1992-04-20");
-  const [gender, setGender] = useState("Female");
-  const [arvAdherence, setArvAdherence] = useState("90157");
-  const [currentRegimen, setCurrentRegimen] = useState("TDF-3TC-DTG");
-  const [indicationForVLTesting, setIndicationForVLTesting] =
-    useState("168684");
+const VLSuppressionPredictionWorkSpace: React.FC<PatientChartProps> = ({
+  patientUuid,
+}) => {
+  const { patient } = usePatient(patientUuid);
 
-  const [patientUuid, setPatientUuid] = useState(
-    "93e4e7e1-c916-47d3-b00d-c7c0aa6d1ce6"
-  );
-  const [conceptUuid, setConceptUuid] = useState(
+  const [indicationError, setIndicationError] = useState("");
+  const [showErrorNotification, setShowErrorNotification] = useState(false);
+
+  const [encounterDate, setEncounterDate] = useState<string | null>("");
+  const handleLastEncounterDateReceived = (newLastEncounterDate: string) => {
+    setEncounterDate(newLastEncounterDate);
+  };
+
+  const [artStartDate, setArtStartDate] = useState("");
+  const handleArtStartDateDataReceived = (newArtStartDate: string) => {
+    setArtStartDate(newArtStartDate);
+  };
+
+  const [currentRegimen, setCurrentRegimen] = useState("");
+  const handleCurrentRegimenReceived = (newCurrentRegimen: string) => {
+    setCurrentRegimen(newCurrentRegimen);
+  };
+
+  const [arvAdherence, setArvAdherence] = useState("");
+  const handleAdherenceReceived = (newAdherenceReceivedRecieved: string) => {
+    setArvAdherence(newAdherenceReceivedRecieved);
+  };
+
+  const [indicationForVLTesting, setIndicationForVLTesting] = useState("");
+  const handleIndicationForVLTestingReceived = (
+    newIndicationForVLTesting: string
+  ) => {
+    if (
+      newIndicationForVLTesting ===
+      "No parameter for last_indication_for_VL_Testing"
+    ) {
+      setIndicationError(newIndicationForVLTesting);
+    } else {
+      setIndicationForVLTesting(newIndicationForVLTesting);
+      setIndicationError("");
+    }
+  };
+
+  useGetARTStartDate(
+    {
+      patientuuid: patientUuid,
+    },
+    handleArtStartDateDataReceived,
     "ab505422-26d9-41f1-a079-c3d222000440"
   );
-  const { artStartDateData, isLoading, conceptuuid, patientuuid } =
-    useGetARTStartDate({
+
+  useGetLastEncounterDate(
+    {
       patientuuid: patientUuid,
-      conceptuuid: conceptUuid,
-    });
+    },
+    handleLastEncounterDateReceived,
+    "59f36196-3ebe-4fea-be92-6fc9551c3a11"
+  );
+
+  useGetCurrentRegimen(
+    {
+      patientuuid: patientUuid,
+    },
+    handleCurrentRegimenReceived,
+    "dd2b0b4d-30ab-102d-86b0-7a5022ba4115"
+  );
+
+  useGetIndicationForVLTesting(
+    { patientuuid: patientUuid },
+    handleIndicationForVLTestingReceived,
+    "59f36196-3ebe-4fea-be92-6fc9551c3a11"
+  );
+
+  useGetAherence(
+    { patientuuid: patientUuid },
+    handleAdherenceReceived,
+    "dce03b2f-30ab-102d-86b0-7a5022ba4115"
+  );
+
+  const gender = useMemo(() => {
+    return patient ? patient.gender : "Female";
+  }, [patient]);
+
+  const dateOfBirth = useMemo(() => {
+    return patient ? extractDate(patient.birthDate) : "";
+  }, [patient]);
+
+  const patientDisplay = useMemo(() => {
+    return patient
+      ? `${patient.name[0].given.join(" ")} ${patient.name[0].family}`
+      : "Patient";
+  }, [patient]);
+
+  const [showPredictions, setshowPredictions] = useState(true);
 
   const { data, isErrorInSendingRequest, isLoadingPrediction } =
     useVLSuppressionDetails({
@@ -40,38 +122,92 @@ const VLSuppressionPredictionWorkSpace: React.FC = () => {
       last_indication_for_VL_Testing: indicationForVLTesting,
     });
 
-  if (isLoading && patientuuid && conceptuuid) {
-    setPatientUuid(patientuuid);
-    setConceptUuid(conceptuuid);
-  }
+  const [validationError, setValidationError] = useState<string | null>(null);
+  useEffect(() => {
+    const error = validateVLSuppressionParams({
+      last_encounter_date: encounterDate,
+      art_start_date: artStartDate,
+      date_birth: dateOfBirth,
+      gender: gender,
+      last_arv_adherence: arvAdherence,
+      current_regimen: currentRegimen,
+      last_indication_for_VL_Testing: indicationForVLTesting,
+    });
+    setValidationError(error);
+  }, [
+    encounterDate,
+    artStartDate,
+    dateOfBirth,
+    gender,
+    arvAdherence,
+    currentRegimen,
+    indicationForVLTesting,
+  ]);
 
-  if (isLoadingPrediction) {
-    console.info("ART START DATE", artStartDateData);
-    console.info("Concept uuid", conceptuuid);
-    console.info("Person uuid", patientuuid);
-    return (
-      <InlineLoading
-        status="active"
-        iconDescription="Loading"
-        description="Loading data..."
-      />
-    );
-  }
-
-  if (isErrorInSendingRequest) {
-    return <ErrorState error={isErrorInSendingRequest} headerTitle={"Error"} />;
-  }
+  const handleCloseNotification = () => {
+    setShowErrorNotification(false);
+  };
 
   return (
-    <div className={styles.divContainer}>
-      <section className={styles.section}>
-        <div className={styles.title}>VL Suppression</div>
-        <div className={styles.divVL}>
-          Prediction: <>{data}</>
+    <>
+      <section className={styles.sectionHeader}>
+        <div className={styles.sectionHeaderContent}>
+          <img src={logo} alt="logo" height={60} />
+          <div>Welcome to Dr. Yonna</div>
+          <p>Your AI Partner</p>
         </div>
+        <span className={styles.divSpan}>
+          An AI based diagnostic assessment for {patientDisplay}
+        </span>
       </section>
-      <section></section>
-    </div>
+      {!validationError ? (
+        <>
+          {isLoadingPrediction && (
+            <InlineLoading
+              status="active"
+              iconDescription="Loading"
+              description="Loading data..."
+            />
+          )}
+          {isErrorInSendingRequest && (
+            <InlineLoading
+              status="active"
+              iconDescription="Loading"
+              description="Getting Patient Details..."
+            />
+          )}
+          {!isLoadingPrediction && !isErrorInSendingRequest && (
+            <>
+              <div className={styles.title}>
+                {patientDisplay} 's Medical History
+              </div>
+              <PatientList />
+              {showPredictions && (
+                <section className={styles.section}>
+                  <div className={styles.divVL}>
+                    Based on the above report, Dr. Yonna predicts that the VL is{" "}
+                    <span className={styles.result}>
+                      <>{data}</>
+                    </span>
+                    .
+                  </div>
+                  <div className={styles.divVL}>
+                    Remember, this prediction is based on a model and should be
+                    used in conjunction with clinical judgment.
+                  </div>
+                </section>
+              )}
+            </>
+          )}
+        </>
+      ) : (
+        <InlineNotification
+          kind="error"
+          onClose={handleCloseNotification}
+          title={validationError}
+        />
+      )}
+    </>
   );
 };
 
--- src/components/workspace/ai-predictions/vl-suppression-prediction.resource.ts
+++ src/components/workspace/ai-predictions/vl-suppression-prediction.resource.ts
@@ -1,8 +1,8 @@
 import useSWR from "swr";
 import axios from "axios";
 import { openmrsFetch } from "@openmrs/esm-framework";
-
-interface SaveParams {
+import { useEffect } from "react";
+type SaveParams = {
   last_encounter_date: string;
   art_start_date: string;
   date_birth: string;
@@ -10,18 +10,29 @@ interface SaveParams {
   last_arv_adherence: string;
   current_regimen: string;
   last_indication_for_VL_Testing: string;
-}
+};
 
-interface PredictionData {
+export interface PredictionData {
   Prediction: {
     Client: string;
   };
 }
 
 type ARTStartDateRequest = {
-  conceptuuid: string;
   patientuuid: string;
 };
+
+export function validateVLSuppressionParams(params: SaveParams): string | null {
+  if (!params.last_encounter_date) return "Patient has no last encounter date";
+  if (!params.art_start_date) return "Patient has no ART start date";
+  if (!params.date_birth) return "Patient has no Date of birth";
+  if (!params.gender) return "Patient has no gender";
+  if (!params.last_arv_adherence) return "Patient has no ARV adherence";
+  if (!params.current_regimen) return "Patient has no current regimen";
+  if (!params.last_indication_for_VL_Testing)
+    return "Patient has no indication for VL Testing";
+  return null;
+}
 
 export function useVLSuppressionDetails(params: SaveParams) {
   const apiUrl = "https://ai.mets.or.ug/predict";
@@ -33,10 +44,14 @@ export function useVLSuppressionDetails(params: SaveParams) {
           "Content-Type": "application/json",
         },
       });
-
+      console.info(response.data);
       return response.data as PredictionData;
     } catch (error) {
-      throw new Error(`Error in fetcher: ${error.message}`);
+      if (error.response.data.error) {
+        throw new Error(error.response.data.error);
+      } else {
+        throw new Error("An unexpected error occurred.");
+      }
     }
   };
 
@@ -44,7 +59,6 @@ export function useVLSuppressionDetails(params: SaveParams) {
     PredictionData,
     Error
   >(apiUrl, fetcher);
-
   return {
     data: data?.Prediction.Client ?? {},
     isErrorInSendingRequest: error,
@@ -53,7 +67,7 @@ export function useVLSuppressionDetails(params: SaveParams) {
   };
 }
 
-function extractDate(timestamp: string): string {
+export function extractDate(timestamp: string): string {
   const dateObject = new Date(timestamp);
   const year = dateObject.getFullYear();
   const month = (dateObject.getMonth() + 1).toString().padStart(2, "0");
@@ -62,19 +76,155 @@ function extractDate(timestamp: string): string {
   return `${year}-${month}-${day}`;
 }
 
-export function useGetARTStartDate(params: ARTStartDateRequest) {
-  const apiUrl = `/ws/rest/v1/obs?concept=${params.conceptuuid}&patient=${params.patientuuid}&v=full`;
-  const { data, error, isLoading, isValidating, mutate } = useSWR<
+export function useGetARTStartDate(
+  params: ARTStartDateRequest,
+  onArtStartDateDataReceived: (artStartDateData: string) => void,
+  conceptuuid: string
+) {
+  const apiUrl = `/ws/rest/v1/obs?concept=${conceptuuid}&patient=${params.patientuuid}&v=full`;
+  const { data, error, isLoading, mutate } = useSWR<
     { data: { results: any } },
     Error
   >(apiUrl, openmrsFetch);
   const artStartDateData = data ? extractDate(data.data.results[0].value) : [];
-  const conceptuuid = data ? data?.data.results[0].concept.uuid : null;
-  const patientuuid = data ? data?.data.results[0].person.uuid : null;
+
+  useEffect(() => {
+    if (artStartDateData !== null) {
+      onArtStartDateDataReceived(artStartDateData as string);
+    }
+  }, [artStartDateData, conceptuuid, onArtStartDateDataReceived]);
+
   return {
     artStartDateData,
     conceptuuid,
-    patientuuid,
+    isError: error,
+    isLoading: isLoading,
+    mutate,
+  };
+}
+
+export function useGetLastEncounterDate(
+  params: ARTStartDateRequest,
+  onLastEncounterReceived: (lastEncounterData: string) => void,
+  conceptuuid: string
+) {
+  const apiUrl = `/ws/rest/v1/encounter?patient=${params.patientuuid}&concept=${conceptuuid}&v=default`;
+  const { data, error, isLoading, mutate } = useSWR<
+    { data: { results: any } },
+    Error
+  >(apiUrl, openmrsFetch);
+
+  useEffect(() => {
+    if (data && data.data.results.length > 0) {
+      const sortedEncounters = data.data.results.sort(
+        (a, b) =>
+          new Date(b.encounterDatetime).getTime() -
+          new Date(a.encounterDatetime).getTime()
+      );
+
+      const lastEncounterData = extractDate(
+        sortedEncounters[0].encounterDatetime
+      );
+      onLastEncounterReceived(lastEncounterData);
+    }
+  }, [data, onLastEncounterReceived]);
+
+  return {
+    lastEncounterData: data
+      ? extractDate(data.data.results[0].encounterDatetime)
+      : [],
+
+    isError: error,
+    isLoading: isLoading,
+    mutate,
+  };
+}
+
+export function useGetCurrentRegimen(
+  params: ARTStartDateRequest,
+  onCurrentARVRegimenReceived: (artStartDateData: string) => void,
+  conceptuuid: string
+) {
+  const apiUrl = `/ws/rest/v1/obs?concept=${conceptuuid}&patient=${params.patientuuid}&v=full`;
+  const { data, error, isLoading, mutate } = useSWR<
+    { data: { results: any } },
+    Error
+  >(apiUrl, openmrsFetch);
+  const currentARVRegimen = data ? data?.data.results[0].value?.display : null;
+
+  useEffect(() => {
+    if (currentARVRegimen !== null) {
+      onCurrentARVRegimenReceived(currentARVRegimen as string);
+    }
+  }, [currentARVRegimen, conceptuuid, onCurrentARVRegimenReceived]);
+  return {
+    currentARVRegimen,
+    conceptuuid,
+    isError: error,
+    isLoading: isLoading,
+    mutate,
+  };
+}
+
+export function useGetIndicationForVLTesting(
+  params: ARTStartDateRequest,
+  onIndicationForVLTestingReceived: (indicationForVLTesting: string) => void,
+  conceptuuid: string
+) {
+  const apiUrl = `/ws/rest/v1/obs?concept=${conceptuuid}&patient=${params.patientuuid}&v=full`;
+  const { data, error, isLoading, mutate } = useSWR<
+    { data: { results: any[] } },
+    Error
+  >(apiUrl, openmrsFetch);
+
+  useEffect(() => {
+    if (error) {
+      onIndicationForVLTestingReceived(
+        "An error occurred while fetching the data."
+      );
+      return;
+    }
+
+    if (data && data.data.results.length > 0 && data.data.results[0].value) {
+      onIndicationForVLTestingReceived(data.data.results[0].value.display);
+    } else {
+      onIndicationForVLTestingReceived(
+        "No parameter for last_indication_for_VL_Testing"
+      );
+    }
+  }, [data, error, onIndicationForVLTestingReceived]);
+
+  return {
+    indicationForVLTesting:
+      data && data.data.results.length > 0
+        ? data.data.results[0].value?.display
+        : null,
+    isError: error,
+    isLoading: isLoading,
+    mutate,
+  };
+}
+
+export function useGetAherence(
+  params: ARTStartDateRequest,
+  onAdherenceReceived: (artStartDateData: string) => void,
+  conceptuuid: string
+) {
+  const apiUrl = `/ws/rest/v1/obs?concept=${conceptuuid}&patient=${params.patientuuid}&v=full`;
+  const { data, error, isLoading, mutate } = useSWR<
+    { data: { results: any } },
+    Error
+  >(apiUrl, openmrsFetch);
+  const adherence = data ? data?.data.results[0].value?.display : null;
+
+  useEffect(() => {
+    if (adherence !== null) {
+      onAdherenceReceived(adherence as string);
+    }
+  }, [adherence, conceptuuid, onAdherenceReceived]);
+  return {
+    adherence,
+    conceptuuid,
     isError: error,
     isLoading: isLoading,
     mutate,
